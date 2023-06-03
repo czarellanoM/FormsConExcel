5543c025126f1ea2ff6cadd84058597f91117b9a
Cristian Arellano <zaidarellano21@gmail.com>
2023-06-03 11:09:21 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # URL de la API de Google Sheets
-URL_DOCS_EXCLE = "https://docs.google.com/spreadsheets/d/1XkKUEurirPNX48lqUfp6KEjz6SL5B2LvEmsVm9dGx4o/gviz/tq?tqx=out:json&gid=758723319"
+URL_DOCS_EXCLE = "https://docs.google.com/spreadsheets/d/ID_DOCUMENTO/gviz/tq?tqx=out:json&gid=ID_HOJA_TRABAJO"
 
 # URL de forms Microsoft
 URL_FORMS_MICROSOFT = "https://forms.office.com/pages/responsepage.aspx?id=_TkAZSpx4U-4Y4YQPEJcF4dtgWGTnWRBjtRobIwR-ApUMVdaVVkzREZCU1JQSExWRU1QN1hNVVNJRy4u"
